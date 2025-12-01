7ee924cd5e5bdd42a239ade0192b3e236dddbc4d
Jonathan Schmitz <jomitz96@hotmail.com>
2025-12-01 19:45:23 +0100

    Rename mult_of_100 to count_mults_of_100 and refactor logic for clarity

--- src/day01/day01.py
+++ src/day01/day01.py
@@ -1,26 +1,28 @@
 from math import ceil, floor
 
-def mult_of_100(a, b):
-    if a > b:
-        a, b = b, a
-    return len(set(range(ceil(a / 100), floor(b / 100) + 1)))
+def count_mults_of_100(a, b):
+    low, high = sorted((a, b))
+    return len(set(range(ceil(low / 100), floor(high / 100) + 1)))
 
 
 with open("input.txt") as f:
     direction_mapping = {"L": -1, "R": 1}
-    val, part_1_counter, part_2_counter = 50, 0, 0
+    prev_val, part_1_counter, part_2_counter = 50, 0, 0
     for line in f.readlines():
         direction, dist = line[0], line[1:]
         offset = direction_mapping[direction] * int(dist)
-        next_val = val + offset
-        if next_val % 100 == 0:
-            part_1_counter += 1
-        part_2_counter += mult_of_100(val, next_val)
+        new_val = prev_val + offset
 
+        # Part 1
+        if new_val % 100 == 0:
+            part_1_counter += 1
+
+        # Part 2
+        part_2_counter += count_mults_of_100(prev_val, new_val)
         # We want to count the new value only once
-        if val % 100 == 0:
+        if prev_val % 100 == 0:
             part_2_counter -= 1
-        val = next_val
+        prev_val = new_val
 
     print(part_1_counter)
     print(part_2_counter)
