62742e9d575611c1fd0026108f5906e7c0a737bd
Jonathan Schmitz <jomitz96@hotmail.com>
2025-12-01 19:41:20 +0100

    Refactor movement logic to include a new function for counting multiples of 100

--- src/day01/day01.py
+++ src/day01/day01.py
@@ -1,3 +1,11 @@
+from math import ceil, floor
+
+def mult_of_100(a, b):
+    if a > b:
+        a, b = b, a
+    return len(set(range(ceil(a / 100), floor(b / 100) + 1)))
+
+
 with open("input.txt") as f:
     direction_mapping = {"L": -1, "R": 1}
     val, part_1_counter, part_2_counter = 50, 0, 0
@@ -5,17 +13,14 @@ with open("input.txt") as f:
         direction, dist = line[0], line[1:]
         offset = direction_mapping[direction] * int(dist)
         next_val = val + offset
-        if next_val <= 0:
-            part_2_counter += abs(next_val // 100)
-            if next_val % 100 == 0:
-                part_2_counter += 1
-            if val == 0:
-                part_2_counter -= 1
-        elif next_val >= 100:
-            part_2_counter += next_val // 100
-        val = next_val % 100
-        if val == 0:
+        if next_val % 100 == 0:
             part_1_counter += 1
+        part_2_counter += mult_of_100(val, next_val)
+
+        # We want to count the new value only once
+        if val % 100 == 0:
+            part_2_counter -= 1
+        val = next_val
 
     print(part_1_counter)
     print(part_2_counter)
